16146f73f9f247ccfca3f048e4277e1debd71798
bigboyKa <amoantwikofistu@gmail.com>
2024-07-27 19:09:35 -0500

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import {Nunito} from "next/font/google";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider/>
+          <RegisterModal />
+          <Navbar/>
+          </ClientOnly>
+        {children}
+        </body>
+    </html>
+  );
+}
